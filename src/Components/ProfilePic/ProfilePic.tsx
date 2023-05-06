041cd68fc878466e5b6ac7c2c92aba1a1e65d4ad
Amir <theamirm@gmail.com>
2023-05-07 01:33:11 +0330

    fixed profilepic address

--- src/Components/ProfilePic/ProfilePic.tsx
+++ src/Components/ProfilePic/ProfilePic.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 export const ProfilePic = (): JSX.Element => {
   return (
-    <div className="grid-profile-pic">
+    <div
+      className="grid-profile-pic"
+      style={{
+        backgroundImage: "url(./assets/defaultPP.svg)"
+      }}>
       <img
         className="glasses"
         src="./assets/glassesDWI.svg"
